c679246c0a60c988e38e3917e441779511a5f09c
arno <arno@6c0b4967-9d8a-4ddb-8c85-6a563999f959>
2009-03-18 15:15:36 +0000

    fixing FollowableFriends relation finder
    git-svn-id: http://code.bermilabs.com/projects/plugins/acts_as_followable@633 6c0b4967-9d8a-4ddb-8c85-6a563999f959

--- installer/files/app/models/followable_friend.php
+++ installer/files/app/models/followable_friend.php
@@ -2,6 +2,13 @@
 Ak::import('Follower');
 class FollowableFriend extends Follower 
 {
+    function __construct()
+    {
+        $this->setTableName('followable_friends', true, true);
+        $attributes = (array)func_get_args();
+        return $this->init($attributes);
+    }
+    
     function _getIdentifier()
     {
         return 'following_id';
